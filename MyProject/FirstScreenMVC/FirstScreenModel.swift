1724238a65940aa27e69947e938aa91eede99f27
Niff1e <niff1e@MacBook-Air-Niff1e.local>
2023-01-08 04:43:21 -0800

    Dowload picture in init

--- MyProject/FirstScreenMVC/FirstScreenModel.swift
+++ MyProject/FirstScreenMVC/FirstScreenModel.swift
@@ -23,13 +23,28 @@ final class FirstScreenModel {
     private var currentImageNumber = 1
     private var pressCount: Int = 0
     private let queue = DispatchQueue(label: "myQueue")
+    private let urls = [
+        "https://mobimg.b-cdn.net/v3/fetch/9d/9db2d4683d92f5f2045e9142fbd82633.jpeg",
+        "https://phonoteka.org/uploads/posts/2021-06/1624471479_22-phonoteka_org-p-anime-oboi-gorizontalnie-krasivo-25.jpg",
+        "https://avatars.mds.yandex.net/i?id=2931c05db613d78b6eaf2f7818eca3f8-5869745-images-thumbs&n=13"
+    ]
 
     // MARK: -
 
     init() {
-        if let image = UIImage(named: "\(currentImageNumber)") {
-            self.image = image
-            self.imageTitle = "Картинка \(currentImageNumber)"
+        if let url = URL(string: urls[currentImageNumber - 1]) {
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    self.image = image
+                    self.imageTitle = "Картинка \(currentImageNumber)"
+                } else {
+                    self.imageTitle = "Smth went wrong"
+                    self.image = UIImage(named: "ErrorCase")!
+                }
+            } else {
+                self.imageTitle = "Smth went wrong"
+                self.image = UIImage(named: "ErrorCase")!
+            }
         } else {
             self.imageTitle = "Smth went wrong"
             self.image = UIImage(named: "ErrorCase")!
